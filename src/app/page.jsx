cc122399ae963abf90ec36de0eb0042189cdd73d
CheSubhro <mitsubhra@gmail.com>
2024-02-14 13:43:07 +0530

    Final Commit

---
+++ src/app/page.jsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<>
+			<h1 className="text-3xl font-bold mt-8 text-center">Subhra Mondal</h1>
+      		<p className="font-semibold text-center">Full Stack Web App Developer</p>
+			<div className="mx-5 mt-2">
+				<p className="text-gray-50 font-semibold bg-zinc-600 p-2 mb-1">Summary</p>
+				<p className="leading-8 tracking-wide"> Welcome to my career portfolio! I am currently a senior full stack web developer at X Technology and I am also open for Side Project. I hope that this portfolio provides insight into my character, programming background, analytical skills, and accomplishments.</p>
+
+				<p className="text-gray-50 font-semibold bg-zinc-600 p-2 mt-3 mb-5">Experience</p>
+				<div className="flex items-center gap-1">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+					<path fillRule="evenodd" d="M12.516 2.17a.75.75 0 00-1.032 0 11.209 11.209 0 01-7.877 3.08.75.75 0 00-.722.515A12.74 12.74 0 002.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 00.374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 00-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08zm3.094 8.016a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+				</svg>
+				<p className="font-semibold">X Technology</p>
+				</div>
+				<p className="ml-7 mb-3">Full Stack Developer (2021 - Present)</p>
+
+				<div className="flex items-center gap-1">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+					<path fillRule="evenodd" d="M12.516 2.17a.75.75 0 00-1.032 0 11.209 11.209 0 01-7.877 3.08.75.75 0 00-.722.515A12.74 12.74 0 002.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 00.374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 00-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08zm3.094 8.016a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+				</svg>
+				<p className="font-semibold">X1 Technology</p>
+				</div>
+				<p className="ml-7 mb-3">Seniour Web Developer  (2019 - 2021)</p>
+
+				<div className="flex items-center gap-1">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+					<path fillRule="evenodd" d="M12.516 2.17a.75.75 0 00-1.032 0 11.209 11.209 0 01-7.877 3.08.75.75 0 00-.722.515A12.74 12.74 0 002.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 00.374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 00-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08zm3.094 8.016a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+				</svg>
+				<p className="font-semibold">X2 Technology</p>
+				</div>
+				<p className="ml-7 mb-3">Backend Developer (2018-2019)</p>
+
+				<div className="flex items-center gap-1">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+					<path fillRule="evenodd" d="M12.516 2.17a.75.75 0 00-1.032 0 11.209 11.209 0 01-7.877 3.08.75.75 0 00-.722.515A12.74 12.74 0 002.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 00.374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 00-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08zm3.094 8.016a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+				</svg>
+				<p className="font-semibold">X3 Technology</p>
+				</div>
+				<p className="ml-7 mb-3">Support Executive (2017-2018)</p>
+
+				<p className="text-gray-50 font-semibold bg-zinc-600 p-2 mt-3 mb-5">Education</p>
+				<div className="flex items-center gap-1">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+					<path fillRule="evenodd" d="M12.516 2.17a.75.75 0 00-1.032 0 11.209 11.209 0 01-7.877 3.08.75.75 0 00-.722.515A12.74 12.74 0 002.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 00.374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 00-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08zm3.094 8.016a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+				</svg>
+				<p className="font-semibold">West Bengal State University</p>
+				</div>
+				<p className="ml-7 mb-3">B.Tech of Computer Application</p>
+				<div className="flex items-center gap-1">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+					<path fillRule="evenodd" d="M12.516 2.17a.75.75 0 00-1.032 0 11.209 11.209 0 01-7.877 3.08.75.75 0 00-.722.515A12.74 12.74 0 002.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 00.374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 00-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08zm3.094 8.016a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
+				</svg>
+				<p className="font-semibold">West Bengal Council Of Technical Education</p>
+				</div>
+				<p className="ml-7 mb-3">Diploma of Computer Application</p>
+
+			</div>
+		</>
+	);
+}
